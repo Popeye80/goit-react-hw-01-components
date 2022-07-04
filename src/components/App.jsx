89c4622d0041d1b0ee1ed9999e1df8b00a236985
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-07-04 08:45:32 +0300

    finish2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
-console.log(user);
+
 export const App = () => {
   return (
     <AppWrap>
